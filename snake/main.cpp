5bbb8b61ed5a51b27e37de55ce4b00e95547230b
yangruiqiyr <yangruiqiyr@163.com>
2018-01-25 09:43:18 +0800

    修改了一下菜单位置

--- snake/main.cpp
+++ snake/main.cpp
@@ -17,16 +17,21 @@ int main(){
 		while (true) {
 			int nNum = -1;
 			system("cls");
-
+			CTools::writeCHar(29, 20);
 			cout << "*******************************" << endl;
+			CTools::writeCHar(29, 21);
 			cout << "*1.地图编辑                   *" << endl;
+			CTools::writeCHar(29, 22);
 			cout << "*2.开始游戏                   *" << endl;
+			CTools::writeCHar(29, 23);
 //			cout << "*3.保存                       *" << endl;
 //			cout << "*5.载入                       *" << endl;
-			cout << "*4.退出                       *" << endl;
+			cout << "*3.退出                       *" << endl;
+			CTools::writeCHar(29, 24);
 			cout << "*******************************" << endl;
+			CTools::writeCHar(29, 25);
 			cin >> nNum;
-			if (nNum == 4)
+			if (nNum == 3)
 				return 0;
 			switch (nNum)
 			{
